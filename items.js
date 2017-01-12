7333d49ffe8479bb5d1bfd6bfdb401ed8aa8e778
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2017-01-11 21:36:24 -0500

    added comments in code; adjusted formula to have score within range

--- items.js
+++ items.js
@@ -2,18 +2,23 @@ document.addEventListener("DOMContentLoaded",function(event){
   var playing = false;
   var score = 0;
 
-  // create items worth 10 to 30 points
+  // create items worth 5 to 20 points
   function low(){
+    // generate random images for low scoring items
     var src = [
       "http://image.flaticon.com/icons/svg/135/135739.svg",
       "http://image.flaticon.com/icons/svg/290/290406.svg",
       "http://image.flaticon.com/icons/svg/135/135559.svg",
       "http://image.flaticon.com/icons/svg/186/186151.svg"
     ];
-
     var imageRandom = Math.round(Math.random() * 3);
-    var pointRandom = Math.round(Math.random() * 30);
 
+    // points
+    var maxPts = 20;
+    var minPts = 5;
+    var pointRandom = Math.round(Math.random() * (maxPts - minPts) + 5);
+
+    // mounting image onto gameboard
     var image = document.createElement('img');
     image.setAttribute('src',src[imageRandom]);
     image.setAttribute('class','item-low');
@@ -23,12 +28,14 @@ document.addEventListener("DOMContentLoaded",function(event){
     return image;
   };
 
+  // setting up the game
   function startGame(){
     for (var i=1; i<=20; i++){
       addNewItem();
     };
   };
 
+  // adding a single item
   function addNewItem(){
     var newItem = low();
 
@@ -47,13 +54,16 @@ document.addEventListener("DOMContentLoaded",function(event){
     newItem.addEventListener('click',removeItem);
   };
 
+  // logic when object removed
   function removeItem(){
     score += parseInt(this.dataset.point);
+    console.log(this.dataset.point);
     displayScore();
     this.remove();
     addNewItem();
   };
 
+  // update score
   function displayScore(){
     if (!document.getElementById('currentScore')){
       var scoreDiv = document.createElement('div');
