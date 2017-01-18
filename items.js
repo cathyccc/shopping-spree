5f0c78ab274cedecc2b0e1e1090f1dd1b90213ae
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2017-01-18 15:53:36 -0500

    have items draggable with interact.js libray

--- items.js
+++ items.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded",function(event){
     // mounting image onto gameboard
     var image = imageDiv(pointRandom);
     image.setAttribute('src',src[imageRandom]);
-    image.setAttribute('class','item-low');
+    image.className = 'draggable item-low';
     image.style.height = '60px';
     return image;
   };
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded",function(event){
 
     var image = imageDiv(pointRandom);
     image.setAttribute('src',src[imageRandom]);
-    image.setAttribute('class','item-med');
+    image.className = 'draggable item-med';
     image.style.height = '50px';
     return image;
   };
@@ -56,7 +56,7 @@ document.addEventListener("DOMContentLoaded",function(event){
 
     var image = imageDiv(pointRandom);
     image.setAttribute('src',src[imageRandom]);
-    image.setAttribute('class','item-high');
+    image.className = 'draggable item-high';
     image.style.height = '40px';
     return image;
   };
@@ -71,12 +71,13 @@ document.addEventListener("DOMContentLoaded",function(event){
   function imageDiv(pointRandom){
     var image = document.createElement('img');
     image.setAttribute('data-point',pointRandom);
+    image.setAttribute('id','item');
     return image;
   };
 
   // setting up the game
   function startGame(){
-    for (var i=1; i<=60; i++){
+    for (var i=1; i<=2; i++){
       addNewItem();
     };
   };
@@ -110,8 +111,33 @@ document.addEventListener("DOMContentLoaded",function(event){
     newItem.style.top = y + '%';
 
     document.getElementById('app').append(newItem);
-    newItem.addEventListener('click',removeItem);
+
+    dragItem();
   };
+
+  function dragItem(){
+    interact('.draggable').draggable({
+      // inertia: true,
+      // restrict: {
+      //   restriction: "parent",
+      //   endOnly: true,
+      //   elementRect: {top:0, left:0, bottom: 1, right: 1}
+      // },
+      onmove: dragMoveListener
+    })
+  };
+
+  function dragMoveListener(event){
+    var target = event.target;
+    var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+    var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    target.style.transform = `translate(${x}px,${y}px)`;
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  };
+
 
   // logic when object removed
   function removeItem(){
@@ -145,7 +171,6 @@ document.addEventListener("DOMContentLoaded",function(event){
     imgCart.setAttribute('src',"http://image.flaticon.com/icons/svg/116/116383.svg");
     cartDiv.append(imgCart);
 
-    console.log(imgCart);
     document.getElementById('app').append(cartDiv);
   };
 
